2b81a64bc7366d9d0e21fd5d871504624a40e71c
Chris Wynne <Chris.Wynne@arup.com>
2023-06-07 21:49:47 +0100

    shell update

--- geoanalysis-env/postgres-gis-network/initdb-postgis.sh
+++ geoanalysis-env/postgres-gis-network/initdb-postgis.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # initdb-postgis.sh
 
+echo "Running initdb-postgis.sh..."
+
 set -e
 
 # Load PostGIS into the database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  echo "Creating PostGIS and PostGIS_Topology extensions..."
   CREATE EXTENSION IF NOT EXISTS postgis;
   CREATE EXTENSION IF NOT EXISTS postgis_topology;
 EOSQL
+
+echo "Finished running initdb-postgis.sh."
